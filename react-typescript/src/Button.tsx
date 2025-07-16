4d4c34d07bf0475a36b42d3aa6d85987e6458c02
yoojanghun <yoojanghun@naver.com>
2025-07-16 14:03:43 +0900

    omit

--- react-typescript/src/Button.tsx
+++ react-typescript/src/Button.tsx
@@ -1,22 +1,18 @@
-// as const를 추가한 후 마우스를 올려보면 
-// const btnTextOptions: readonly ["Click me!", "Click me again!", "Click me one more time!"]
-// 위처럼 readonly 튜플로 인식됨(값 변경 불가)
+// Omit을 이용하여 특정 key를 제외한 타입 생성 가능
+// 아래에선 User타입에서 name key를 제외한 Guest 타입 생성
 
-// as const 추가 안하면 btnTextOptions는 그냥 string[]으로 추론함
+type User = {
+    sessionId: string;
+    name: string;
+}
+
+type Guest = Omit<User, "name">;
 
 function Button() {
 
-    const btnTextOptions = [
-        "Click me!", 
-        "Click me again!",
-        "Click me one more time!"
-    ] as const;
-
     return(
         <button>
-            {btnTextOptions.map(option => {
-                return option;
-            })}
+            Click me!
         </button>    
     );
 }
