b03ed2d3074f5ec699a3d706b6a0024bda042ad9
yoojanghun <yoojanghun@naver.com>
2025-07-22 13:29:06 +0900

    import type

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -3,7 +3,7 @@ import Button from "./Button.tsx";
 function App() {
 
   return (
-      <Button count={5} countHistory={[12, 63, 21, 87]}/>
+      <Button color="red" />
   );
 }
 
--- react-typescript/src/Button.tsx
+++ react-typescript/src/Button.tsx
@@ -1,15 +1,17 @@
 // count와 countHistory의 타입을 공통된 타입으로 연결하고 싶을 때 generic 사용
+// App.tsx에서 count와 countHistory안의 값은 공통된 type이어야 함.
 
-type ButtonProp<T> = {
-    count: T;
-    countHistory: T[];
+import { type Color } from "./lib/types";
+
+type ButtonProp = {
+    color: Color;
 }
 
-function Button<T>({ count, countHistory }: ButtonProp<T>) {
+function Button({ color }: ButtonProp) {
 
     return(
-        <h1>
-            {String(count)}, {countHistory.map(c => String(c)).join(", ")}
+        <h1 style={{color}}>
+            안녕
         </h1>
     );
 }
